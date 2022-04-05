2995b0ae52bc852ff57068f85ce8994bd922d5ae
Vankhiem <khiemle.pix@gmail.com>
2022-04-05 19:14:23 +0700

    upload file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "antd/dist/antd.css";
+import { Table, Spin, Button, Form, Input, Checkbox } from "antd";
+import { GET_DATA } from "./config";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [load, setLoad] = useState(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setLoad(true);
+        const values = await axios.get(GET_DATA);
+        setData(values.data.results);
+        setLoad(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+
+  console.log(data);
+
+  const dataSource = [
+    ...data.map((item) => {
+      return {
+        key: item.key,
+        name: item.name,
+        description: item.description,
+      };
+    }),
+  ];
+
+  console.log(dataSource);
+
+  const columns = [
+    {
+      title: "Key",
+      dataIndex: "key",
+      key: "key",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "description",
+      dataIndex: "description",
+      key: "description",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Button className="">Add Item</Button>
+      <div className="mt-5 max-w-[500px] mx-auto">
+        <Form
+          name="basic"
+          initialValues={{ remember: true }}
+          autoComplete="off"
         >
-          Learn React
-        </a>
-      </header>
+          <Form.Item
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input placeholder="Key" />
+          </Form.Item>
+
+          <Form.Item
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input placeholder="Name" />
+          </Form.Item>
+          <Form.Item
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input placeholder="Description" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+      <Table
+        dataSource={dataSource}
+        loading={load ? true : false}
+        columns={columns}
+        pagination
+      ></Table>
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,3 @@
+const GET_DATA = "https://api-bms-dev.webdigital.vn/cms/api/v1/sports/";
+
+export { GET_DATA };
